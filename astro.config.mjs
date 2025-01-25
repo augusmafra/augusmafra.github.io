aa635d1a62b6fdbacf4d010115c3a4208d575f04
Augusto Mafra <agus.dataestudio@gmail.com>
2025-01-25 09:43:42 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://augusmafra.github.io',
   devToolbar: {
     enabled: false
   },
