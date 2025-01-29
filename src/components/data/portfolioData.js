63efb5d5a28f71e7de8568e14d1ed8499f72275a
Augusto Mafra <agus.dataestudio@gmail.com>
2025-01-29 01:29:04 +0000

    dashboard spoty publico

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -17,7 +17,7 @@
  */
 export const portafolioData = [
 	{
-		imgSrc: 'https://app.powerbi.com/reportEmbed?reportId=23bde789-8330-487c-a942-4f4c61b9585a&autoAuth=true&ctid=e4caf3d5-f1de-428a-b9e0-2c313a61b94e',
+		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
 		title: 'Dashboard Power BI',
 		skills: ['React', 'StyledComponents'],
 		descripcion:
